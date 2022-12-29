7494b74ecc41b8def73be10fb6220c33cbe8ef84
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2022-12-29 15:18:55 -0300

    Added content to the genres table

---
+++ GamesAPI/Migrations/20221229172645_GenrePopulating.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamesAPI.Migrations
+{
+    public partial class GenrePopulating : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Genres (GenreName) VALUES ('FPS')");
+            mb.Sql("INSERT INTO Genres (GenreName) VALUES ('RPG')");
+            mb.Sql("INSERT INTO Genres (GenreName) VALUES ('RTS')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Genres");
+        }
+    }
+}
